3cbaf6f8ea9ea1d6f7652373d5d7a8c1c317ae47
Sasha Hayles <sashahayles@yahoo.com>
2019-10-20 00:11:16 -0400

    Attempt to close accordion

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 var accordionTitle = document.querySelectorAll('#accordion .accordion-header');
 var accordDesc =document.querySelectorAll('#accordion .open-p')
+
+//accordion dropdown
 function openAccordion() {
+
   //toogle indicator
   for (i=0; i <accordionTitle.length; i++) {
     accordionTitle[i].childNodes[3].innerHTML = 'v';
+    accordDesc[i].classList.remove('accordion-open');
   }
+
+  this.nextElementSibling.classList.toggle ('accordion-open');
   this.childNodes[3].innerHTML ='^';
 }
 //event listener
